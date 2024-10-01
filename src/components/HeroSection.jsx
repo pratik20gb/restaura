e4b34e11725a37d8c01205cf11df6c4d3aac2c6c
Pratik Raj <pratikrajcpr@gmail.com>
2024-10-01 11:20:30 +0530

    added poster

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ function HeroSection() {
     return (
         <section className='relative flex h-screen items-center justify-center'>
             <div className='absolute inset-0 -z-20 h-full w-full overflow-hidden'>
-                <video src={video} className='h-full w-full object-cover' muted autoPlay loop playsInline ></video>
+                <video src={video} className='h-full w-full object-cover' muted autoPlay loop playsInline poster={hero} ></video>
             </div>
             <div className='absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black'></div>
             <div className='relative z-20 flex h-screen flex-col justify-end pb-20'>
